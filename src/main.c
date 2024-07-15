f843f8e05cb0677e3d9d8754af305b1a8aff4634
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-07-15 18:05:18 -0400

    feat: movement and limits

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include "include/raylib.h"
 
+const int screen_width = 1200;
+const int screen_height = 800;
+
+void movement(Vector2 *center) {
+  if (IsKeyDown(KEY_RIGHT) && center->x < (screen_width - 45)) {
+    center->x += 4.0f;
+  }
+  if (IsKeyDown(KEY_LEFT) && center->x > 45) {
+    center->x -= 4.0f;
+  }
+  if (IsKeyDown(KEY_UP) && center->y > 45) {
+    center->y -= 4.0f;
+  }
+  if (IsKeyDown(KEY_DOWN) && center->y < (screen_height - 45)) {
+    center->y += 4.0f;
+  }
+}
+
 int main() {
-  const int screen_width = 1200;
-  const int screen_height = 800;
 
   Rectangle touch_area = {20, 20, screen_width - 40, screen_height - 40};
 
@@ -15,18 +31,16 @@ int main() {
   float end_angle = 300;
 
   while (!WindowShouldClose()) {
-    if (IsKeyDown(KEY_RIGHT))
-      center.x += 2.0f;
-    if (IsKeyDown(KEY_LEFT))
-      center.x -= 2.0f;
-    if (IsKeyDown(KEY_UP))
-      center.y -= 2.0f;
-    if (IsKeyDown(KEY_DOWN))
-      center.y += 2.0f;
+    movement(&center);
 
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawRectangleLinesEx(touch_area, 10, Fade(LIGHTGRAY, 0.6f));
+    DrawRectangleLinesEx(touch_area, 10, Fade(BLUE, 0.6f));
+
+    Rectangle rec = {200, 590, 100, 100};
+
+    DrawRectangleRoundedLines(rec, .25, 5, 5, BLUE);
+
     DrawCircleSector(center, 10, 0, end_angle, 20, YELLOW);
     EndDrawing();
   }
